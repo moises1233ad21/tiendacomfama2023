b2bc9fbb576eb13529db39ce92c3c74108a3cde1
205 <205@academicos.cesde.edu.co>
2023-02-28 20:38:20 -0500

    se crea conexion1

---
+++ proyectoComfama2/tiendadonpepito/src/main/java/com/example/tiendadonpepito/TiendadonpepitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiendadonpepito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendadonpepitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendadonpepitoApplication.class, args);
+	}
+
+}
---
+++ proyectoComfama2/tiendadonpepito/src/test/java/com/example/tiendadonpepito/TiendadonpepitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiendadonpepito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendadonpepitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
